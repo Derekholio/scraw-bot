1ae60155cf322a4a94f5d89fc0dba7d8ddef74d9
Derek Sims <derekholio1@gmail.com>
2018-04-08 00:38:49 -0500

    initial commit

---
+++ install.bat
@@ -0,0 +1,6 @@
+npm install erlpack@discordapp/erlpack
+npm install node-opus@^0.2.7
+npm install opusscript@^0.0.6
+npm install sodium@^2.0.3
+npm install libsodium-wrappers@^0.7.3
+npm install uws@^9.14.0
